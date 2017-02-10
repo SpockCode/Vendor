1256f6a6e1173307d6b1fbe5bf20ec9572658355
SpockCode <obasi.chimdinma@gmail.com>
2017-02-10 12:46:47 +0100

    first commit

---
+++ js/dashboard.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+            $("#dashboard").show();
+        $("#services").hide();
+         $("#vendor").hide();
+         $("#session").hide();
+         $("#ManageShifts").hide();
+         $("#messages").hide();
+         $("#report").hide();    
+        
+    
+});
+
+
+
+$(document).ready(function(){
+    $("#navdashboard").click(function(){
+        $("#dashboard").show();
+        $("#services").hide();
+         $("#vendor").hide();
+         $("#session").hide();
+         $("#ManageShifts").hide();
+         $("#messages").hide();
+         $("#report").hide();    
+        
+    });
+});
+
+
+$(document).ready(function(){
+    $("#navservices").click(function(){
+        $("#services").show();
+        $("#dashboard").hide();
+         $("#vendor").hide();
+         $("#session").hide();
+         $("#ManageShifts").hide();
+         $("#messages").hide();
+         $("#report").hide();    
+        
+    });
+});
+
+$(document).ready(function(){
+    $("#navvendor").click(function(){
+        $("#vendor").show();
+        $("#dashboard").hide();
+         $("#services").hide();
+         $("#session").hide();
+         $("#ManageShifts").hide();
+         $("#messages").hide();
+         $("#report").hide();    
+        
+    });
+});
+
+$(document).ready(function(){
+    $("#navsession").click(function(){
+        $("#session").show();
+        $("#dashboard").hide();
+         $("#services").hide();
+         $("#vendor").hide();
+         $("#ManageShifts").hide();
+         $("#messages").hide();
+         $("#report").hide();    
+        
+    });
+});
+
+
+$(document).ready(function(){
+    $("#navManageShifts").click(function(){
+        $("#ManageShifts").show();
+        $("#dashboard").hide();
+         $("#services").hide();
+         $("#vendor").hide();
+         $("#session").hide();
+         $("#messages").hide();
+         $("#report").hide();    
+        
+    });
+});
+
+$(document).ready(function(){
+    $("#navmessages").click(function(){
+        $("#messages").show();
+        $("#dashboard").hide();
+         $("#services").hide();
+         $("#vendor").hide();
+         $("#session").hide();
+         $("#ManageShifts").hide();
+         $("#report").hide();    
+        
+    });
+});
+
+$(document).ready(function(){
+    $("#navreport").click(function(){
+        $("#report").show();
+        $("#dashboard").hide();
+         $("#services").hide();
+         $("#vendor").hide();
+         $("#session").hide();
+         $("#ManageShifts").hide();
+         $("#messages").hide();    
+        
+    });
+});
